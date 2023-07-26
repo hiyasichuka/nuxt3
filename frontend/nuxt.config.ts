12c15d7fecf689e463275301cb821835d0a4480b
Kei Takahashi <takahashi.kgs@om.asahi-kasei.co.jp>
2023-07-26 11:26:50 +0900

    setup github pages

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,6 +1,16 @@
+import path from 'path'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  // ssr: false,
+  ssr: true,
+  nitro: {
+    output: {
+      publicDir: path.join(__dirname, 'docs')
+    }
+  },
+  app: {
+    baseURL: '/nuxt3/'
+  },
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss']
 })
