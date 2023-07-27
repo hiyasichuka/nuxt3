cab1fb8cb6798ed718ff814fff36252295a3e65a
Kei Takahashi <takahashi.kgs@om.asahi-kasei.co.jp>
2023-07-27 09:18:31 +0900

    Fix link

--- frontend/components/AppHeader.vue
+++ frontend/components/AppHeader.vue
@@ -17,10 +17,10 @@
           >
         </a>
         <div class="flex items-center lg:order-2">
-          <a
-            href="/"
+          <NuxtLink
+            to="/"
             class="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
-            >Log in</a
+            >Log in</NuxtLink
           >
           <a
             href="#"
@@ -69,25 +69,18 @@
             class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0"
           >
             <li>
-              <a
-                href="/"
+              <NuxtLink
+                to="/"
                 class="block py-2 pr-4 pl-3 text-white rounded bg-blue-700 lg:bg-transparent lg:text-blue-700 lg:p-0 dark:text-white"
                 aria-current="page"
-                >Home</a
+                >Home</NuxtLink
               >
             </li>
             <li>
-              <a
-                href="#"
+              <NuxtLink
+                to="/about"
                 class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
-                >Company</a
-              >
-            </li>
-            <li>
-              <a
-                href="/about"
-                class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
-                >About</a
+                >About</NuxtLink
               >
             </li>
           </ul>
