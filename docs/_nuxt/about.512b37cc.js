8e7a5fc721285866c61c24d7080ed4439cd4c46b
Kei Takahashi <takahashi.kgs@om.asahi-kasei.co.jp>
2023-07-27 09:05:58 +0900

    run generate

--- docs/_nuxt/about.512b37cc.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,l as t,k as o,m as s}from"./entry.6d63054f.js";const a={},c=s("p",null,"This page will be displayed at the /about route.",-1),n=[c];function r(_,l){return o(),t("section",null,n)}const d=e(a,[["render",r]]);export{d as default};
